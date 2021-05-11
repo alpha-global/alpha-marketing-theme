70fb9d33e076424bdc31841f39b08852d56c1c15
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-11 12:28:34 +0300

    ACF Hero Block

--- wp-content/themes/alpha/inc/acf-fields.php
+++ wp-content/themes/alpha/inc/acf-fields.php
@@ -8,6 +8,7 @@
 if ( function_exists( 'acf_add_local_field_group' ) ) :
 
 	require_once __DIR__ . '/acf-fields/alpha_cta_widget__group_60951528b410f.php';
+	require_once __DIR__ . '/acf-fields/hero_block__group_60991cc8140e6.php';
 	require_once __DIR__ . '/acf-fields/menu_item__group_6094e09d21d9b.php';
 	require_once __DIR__ . '/acf-fields/menu_widget__group_609511d8820ec.php';
 
