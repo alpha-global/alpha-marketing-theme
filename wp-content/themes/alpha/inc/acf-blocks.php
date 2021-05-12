24fb96b6103a509123b4a3ee78e64fc23ecad10f
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-12 19:51:34 +0300

    Stats block

--- wp-content/themes/alpha/inc/acf-blocks.php
+++ wp-content/themes/alpha/inc/acf-blocks.php
@@ -17,7 +17,27 @@ function alpha_register_acf_block_types() {
 			'description'     => __( 'Hero' ),
 			'render_template' => 'template-parts/blocks/hero.php',
 			'category'        => 'alpha',
-			'keywords'        => array( 'hero' ),
+			'keywords'        => array( 'hero', 'alpha' ),
+			'mode'            => 'auto',
+			'icon'            => 'images-alt2',
+			'supports'        => array(
+				'mode'            => false,
+				'align'           => false,
+				'anchor'          => true,
+				'customClassName' => true,
+				'jsx'             => true,
+			),
+		)
+	);
+
+	acf_register_block_type(
+		array(
+			'name'            => 'stats',
+			'title'           => __( 'Stats' ),
+			'description'     => __( 'Stats' ),
+			'render_template' => 'template-parts/blocks/stats.php',
+			'category'        => 'alpha',
+			'keywords'        => array( 'stats', 'alpha' ),
 			'mode'            => 'auto',
 			'icon'            => 'images-alt2',
 			'supports'        => array(
--- wp-content/themes/alpha/inc/acf-fields.php
+++ wp-content/themes/alpha/inc/acf-fields.php
@@ -11,5 +11,6 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 	require_once __DIR__ . '/acf-fields/hero_block__group_60991cc8140e6.php';
 	require_once __DIR__ . '/acf-fields/menu_item__group_6094e09d21d9b.php';
 	require_once __DIR__ . '/acf-fields/menu_widget__group_609511d8820ec.php';
+	require_once __DIR__ . '/acf-fields/stats_block__group_609b97bc65d5b.php';
 
 endif;
