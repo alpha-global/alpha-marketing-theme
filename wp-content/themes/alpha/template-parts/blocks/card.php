9824ffbdf4533bedf59928bcdd6b5612af29e28a
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-20 13:08:15 +0300

    Card Block advanced style: plain Icons support

--- wp-content/themes/alpha/template-parts/blocks/card.php
+++ wp-content/themes/alpha/template-parts/blocks/card.php
@@ -18,15 +18,39 @@ if ( ! empty( $block['className'] ) ) {
 if ( ! empty( $block['align'] ) ) {
 	$class_name .= ' align' . $block['align'];
 }
+if ( get_field( 'style' ) ) {
+	$class_name .= ' has-style has-style-' . get_field( 'style' );
+}
+if ( get_field( 'icon' ) ) {
+	$class_name .= ' has-icon';
+}
+
 
 $allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/image', 'core/button', 'core/buttons', 'core/embed' );
+
+$template = array(
+	array(
+		'core/heading',
+		array(
+			'level'   => 4,
+			'content' => 'Card Title',
+		),
+	),
+	array(
+		'core/paragraph',
+		array(
+			'content' => 'Card Content',
+		),
+	),
+);
 
 ?>
 
 <section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
-	<div class="container">
-		<div class="card-content">
-			<InnerBlocks allowedBlocks="<?php echo esc_attr( wp_json_encode( $allowed_blocks ) ); ?>"/>
-		</div>
+	<div class="card-content">
+		<?php if ( get_field( 'icon' ) ) : ?>
+			<span class="alpha-card-icon aicon-<?php echo esc_attr( get_field( 'icon' ) ); ?>"></span>
+		<?php endif; ?>
+		<InnerBlocks template="<?php echo esc_attr( wp_json_encode( $template ) ); ?>" allowedBlocks="<?php echo esc_attr( wp_json_encode( $allowed_blocks ) ); ?>"/>
 	</div>
 </section>
