0ccfb71e59171acd83abeb296710cba6ff5f6b03
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-20 11:07:40 +0300

    removed container in hero block

--- wp-content/themes/alpha/template-parts/blocks/hero.php
+++ wp-content/themes/alpha/template-parts/blocks/hero.php
@@ -47,29 +47,27 @@ if ( get_field( 'background_image' ) ) {
 
 <section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
 	<div class="background"></div>
-	<div class="container">
-		<div class="alpha-hero-content">
-			<?php if ( get_field( 'title' ) ) : ?>
-				<h1><?php the_field( 'title' ); ?></h1>
-			<?php endif; ?>
-			<?php if ( get_field( 'description' ) ) : ?>
-				<div class="alpha-hero-description"><?php the_field( 'description' ); ?></div>
-			<?php endif; ?>
-			<?php if ( have_rows( 'buttons' ) ) : ?>
-				<ul class="alpha-hero-buttons">
-					<?php
-					while ( have_rows( 'buttons' ) ) :
-						the_row();
-						$button = get_sub_field( 'button' );
-						$icon   = get_sub_field( 'icon' );
-						$style  = get_sub_field( 'style' );
-						?>
-						<li>
-							<?php alpha_button( $button, $style, $icon ); ?>
-						</li>
-					<?php endwhile; ?>
-				</ul>
-			<?php endif; ?>
-		</div>
+	<div class="alpha-hero-content">
+		<?php if ( get_field( 'title' ) ) : ?>
+			<h1><?php the_field( 'title' ); ?></h1>
+		<?php endif; ?>
+		<?php if ( get_field( 'description' ) ) : ?>
+			<div class="alpha-hero-description"><?php the_field( 'description' ); ?></div>
+		<?php endif; ?>
+		<?php if ( have_rows( 'buttons' ) ) : ?>
+			<ul class="alpha-hero-buttons">
+				<?php
+				while ( have_rows( 'buttons' ) ) :
+					the_row();
+					$button = get_sub_field( 'button' );
+					$icon   = get_sub_field( 'icon' );
+					$style  = get_sub_field( 'style' );
+					?>
+					<li>
+						<?php alpha_button( $button, $style, $icon ); ?>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+		<?php endif; ?>
 	</div>
 </section>
