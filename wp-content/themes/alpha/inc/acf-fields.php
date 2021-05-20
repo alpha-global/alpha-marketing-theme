f43a9482296441d5f3e45c4e31f981bb493f825d
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-20 13:07:42 +0300

    ACF Card Block Settings

--- wp-content/themes/alpha/inc/acf-fields.php
+++ wp-content/themes/alpha/inc/acf-fields.php
@@ -9,6 +9,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 
 	require_once __DIR__ . '/acf-fields/accordion_block__group_60a4e9945d0bd.php';
 	require_once __DIR__ . '/acf-fields/alpha_cta_widget__group_60951528b410f.php';
+	require_once __DIR__ . '/acf-fields/card_block__group_60a630fdaa1fc.php';
 	require_once __DIR__ . '/acf-fields/hero_block__group_60991cc8140e6.php';
 	require_once __DIR__ . '/acf-fields/menu_item__group_6094e09d21d9b.php';
 	require_once __DIR__ . '/acf-fields/menu_widget__group_609511d8820ec.php';
---
+++ wp-content/themes/alpha/inc/acf-fields/card_block__group_60a630fdaa1fc.php
@@ -0,0 +1,73 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_60a630fdaa1fc',
+	'title' => 'Card Block',
+	'fields' => array(
+		array(
+			'key' => 'field_60a63108cac47',
+			'label' => 'Style',
+			'name' => 'style',
+			'type' => 'select',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array(
+				'shadow' => 'Shadow Round Corners',
+				'plain' => 'Plain',
+			),
+			'default_value' => 'shadow',
+			'allow_null' => 0,
+			'multiple' => 0,
+			'ui' => 0,
+			'return_format' => 'value',
+			'ajax' => 0,
+			'placeholder' => '',
+		),
+		array(
+			'key' => 'field_60a632de3adb1',
+			'label' => 'Icon',
+			'name' => 'icon',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'block',
+				'operator' => '==',
+				'value' => 'acf/card',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+endif;
