3e018bdc3ddd426ef4033c6bac5659675b3c4b70
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-26 12:48:50 +0300

    Blog archive

---
+++ wp-content/themes/alpha/home.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Blog template
+ *
+ * @package Alpha
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main blog-archive">
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+			if ( is_home() && ! is_front_page() ) :
+				the_post();
+
+				if ( has_post_thumbnail() ) {
+					$background_id         = get_post_thumbnail_id();
+					$background_url        = wp_get_attachment_image_url( $background_id, 'full' );
+					$background_url_mobile = wp_get_attachment_image_url( $background_id, 'large' );
+
+					?>
+					<style>
+						#alpha-blog-hero .background {
+							background-image: url( <?php echo esc_url( $background_url ); ?> );
+						}
+						@media (max-width: 480px) {
+							#alpha-blog-hero .background {
+								background-image: url( <?php echo esc_url( $background_url_mobile ); ?> );
+							}
+						}
+					</style>
+					<?php
+
+				}
+				?>
+			<div class="entry-content">
+				<section id="alpha-blog-hero" class="alpha-hero alpha-hero-full-page">
+					<div class="background"></div>
+					<div class="alpha-hero-content alpha-hero-blog-card">
+						<h3><?php _e( 'Blog', 'alpha' ); ?></h3>
+						<div class="alpha-hero-blog-summary">
+							<div class="date"><?php the_time( 'M j, Y' ); ?></div>
+							<h2><?php the_title(); ?></h2>
+						</div>
+						<p><a href="<?php the_permalink(); ?>"><?php _e( 'Read Article', 'alpha' ); ?></a></p>
+					</div>
+				</section>
+
+			</div>
+
+			<div class="all-posts-header">
+				<h2><?php _e( 'All Posts', 'alpha' ); ?></h2>
+			</div>
+
+			<?php endif; ?>
+
+			
+		<div class="posts">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
+				
+				<div class="post">
+					<?php get_template_part( 'template-parts/content-post-summary' ); ?>
+				</div>
+
+			<?php endwhile; ?>
+		</div>
+
+		<?php get_sidebar(); ?>
+		<?php endif; ?>
+	</main><!-- #main -->
+
+<?php
+get_footer();
