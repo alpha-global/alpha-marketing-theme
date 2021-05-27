088132e6153e02278d2a52759706229dcbca9314
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-27 15:06:26 +0300

    Archive template

--- wp-content/themes/alpha/archive.php
+++ wp-content/themes/alpha/archive.php
@@ -10,32 +10,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main blog-archive">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<div class="all-posts-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</div>
 
+			<?php get_template_part( 'template-parts/list-posts' ); ?>
+
+			<?php get_sidebar(); ?>
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
 
 		else :
 
@@ -47,5 +36,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
