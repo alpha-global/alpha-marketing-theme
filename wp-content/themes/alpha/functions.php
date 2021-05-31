7d6fd57f0a4c26a1b4b6913ecea48699455b3844
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-05-31 13:41:01 +0300

    Fixed Video Popup dependency

--- wp-content/themes/alpha/functions.php
+++ wp-content/themes/alpha/functions.php
@@ -169,7 +169,7 @@ function alpha_scripts() {
 
 	wp_enqueue_script( 'alpha-navigation', get_template_directory_uri() . '/js/navigation.js', array(), $version, true );
 	wp_enqueue_script( 'alpha-buttons', get_template_directory_uri() . '/js/buttons.js', array(), $version, true );
-	wp_enqueue_script( 'alpha-video-popup', get_template_directory_uri() . '/js/video-popup.js', array(), 0, true );
+	wp_enqueue_script( 'alpha-video-popup', get_template_directory_uri() . '/js/video-popup.js', array( 'jquery' ), 0, true );
 
 	if ( has_block( 'acf/testimonials' ) || has_reusable_block( 'acf/testimonials' ) ) {
 		wp_enqueue_script( 'slick', get_template_directory_uri() . '/js/vendor/slick/slick/slick.js', array( 'jquery' ), $version, true );
