ee0e66f064b04346ff80efd2a9087f8d06926127
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-06-01 15:20:14 +0300

    Story post type code

--- wp-content/themes/alpha/inc/story-post-type.php
+++ wp-content/themes/alpha/inc/story-post-type.php
@@ -7,9 +7,10 @@
 
 /**
  * Registers a new post type
+ *
  * @uses $wp_post_types Inserts new post type object into the list
  *
- * @param string $post_type Post type key, must not exceed 20 characters
+ * @param string       $post_type Post type key, must not exceed 20 characters
  * @param array|string $args See optional args description above.
  * @return object|WP_Error the registered post type object, or an error object
  */
