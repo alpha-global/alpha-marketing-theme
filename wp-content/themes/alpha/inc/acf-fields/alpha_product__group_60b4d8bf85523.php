35ee894587be1d0888a0d5304c6751169bf67e19
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-06-01 15:23:11 +0300

    ACF Product fields

---
+++ wp-content/themes/alpha/inc/acf-fields/alpha_product__group_60b4d8bf85523.php
@@ -0,0 +1,122 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_60b4d8bf85523',
+	'title' => 'Alpha Product',
+	'fields' => array(
+		array(
+			'key' => 'field_60b4d95485496',
+			'label' => 'Teaser Video',
+			'name' => 'teaser_video',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_60b4d96585497',
+			'label' => 'Subtitle',
+			'name' => 'subtitle',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_60b4d9ab85499',
+			'label' => 'Product Meta',
+			'name' => 'product_meta',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => 'field_60b4d9e18549b',
+			'min' => 0,
+			'max' => 0,
+			'layout' => 'block',
+			'button_label' => 'Add Item',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_60b4d9d88549a',
+					'label' => 'Icon',
+					'name' => 'icon',
+					'type' => 'alpha_icon',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'font_size' => 14,
+				),
+				array(
+					'key' => 'field_60b4d9e18549b',
+					'label' => 'Text',
+					'name' => 'text',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+				),
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'product',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+endif;
