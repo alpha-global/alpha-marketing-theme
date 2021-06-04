1824962201b1fcc1f14932ef8af8463848f7d8bb
Max Lyuchin <maxim.lyuchin@gmail.com>
2021-06-04 12:29:44 +0300

    Custom "more" link in related products

--- wp-content/themes/alpha/template-parts/content-post-summary.php
+++ wp-content/themes/alpha/template-parts/content-post-summary.php
@@ -16,6 +16,10 @@
 	<div class="post-summary-content">
 		<div class="post-summary-date"><?php the_time( 'M j, Y' ); ?></div>
 		<h6 class="post-summary-title"><?php the_title(); ?></h6>
-		<div class="post-summary-link"><?php _e( 'Read Article', 'alpha' ); ?></div>
+		<?php if ( 'product' === get_post_type() ) : ?>
+			<div class="post-summary-link"><?php _e( 'See More', 'alpha' ); ?></div>
+		<?php else: ?>
+			<div class="post-summary-link"><?php _e( 'Read Article', 'alpha' ); ?></div>
+		<?php endif; ?>
 	</div>
 </a>
