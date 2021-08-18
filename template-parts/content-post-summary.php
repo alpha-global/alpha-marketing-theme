e8058d1747129a6ff49e9bd0082eb63e1acc9dad
Max Lysakovsky <max.lysakovsky@alpha.org>
2021-08-18 13:57:51 -0300

    add feature image fallback custom field

--- template-parts/content-post-summary.php
+++ template-parts/content-post-summary.php
@@ -6,13 +6,24 @@
  */
 
 ?>
-
 <a href="<?php the_permalink(); ?>" class="post-summary">
-	<?php if ( has_post_thumbnail() ) : ?>
+	<?php 
+	
+	// load alternative image from ACF custom field
+	$fallback_image = get_field( 'feature_image_fallback' );
+	
+	if( $fallback_image ): ?>
+		<div class="post-summary-thumbnail">
+			<?php echo ( wp_get_attachment_image( $fallback_image, 'large' ) ); ?>
+		</div>
+
+	<?php elseif( has_post_thumbnail() ) : ?>
 		<div class="post-summary-thumbnail">
 			<?php the_post_thumbnail( 'large' ); ?>
 		</div>
 	<?php endif; ?>
+
+
 	<div class="post-summary-content">
 		<div class="post-summary-date"><?php the_time( 'M j, Y' ); ?></div>
 		<h6 class="post-summary-title"><?php the_title(); ?></h6>
